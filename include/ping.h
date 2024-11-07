9254e22b235801a3bee3e862882e3bfd2292f611
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2024-11-07 20:40:21 +0100

    Add docs to header

--- include/ping.h
+++ include/ping.h
@@ -1,6 +1,15 @@
 #ifndef PING_H
 #define PING_H
 
-int ping(char *, int);
+/**
+ * @brief Pings an IPv4 or IPv6 address.
+ *
+ * @param address The IPv4 or IPv6 address string to ping. Must be a valid
+ * format, e.g., "93.184.215.14" or "2606:2800:21f:cb07:6820:80da:af6b:8b2c".
+ * @param tries The number of echo requests to send.
+ * @return int 0 if host is reachable within the number specified tries.
+ * -1 if host is unreachable or if an error occurs (e.g., invalid address).
+ */
+int ping(char *address, int tries);
 
 #endif
