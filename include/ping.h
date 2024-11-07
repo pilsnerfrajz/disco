3cc7d102a62a7463b77b1c6c6fb4f86ffbdda2a2
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2024-11-07 21:36:44 +0100

    Update ping.h

--- include/ping.h
+++ include/ping.h
@@ -6,8 +6,9 @@
  *
  * @param address The IPv4 or IPv6 address string to ping. Must be a valid
  * format, e.g., "93.184.215.14" or "2606:2800:21f:cb07:6820:80da:af6b:8b2c".
+ * Hostnames or domain names are not valid arguments.
  * @param tries The number of echo requests to send.
- * @return int 0 if host is reachable within the number specified tries.
+ * @return `int` 0 if host is reachable within the number specified tries.
  * -1 if host is unreachable or if an error occurs (e.g., invalid address).
  */
 int ping(char *address, int tries);
