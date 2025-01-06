3d1fd24359ef1e97ab674ce797ca921f3a22234b
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-06 22:28:03 +0100

    Add comments

--- include/arp.h
+++ include/arp.h
@@ -3,18 +3,30 @@
 
 #include <sys/types.h>
 
-/* RFC 826 */
+/**
+ * @brief ARP packet according to RFC 826. Allocate memory for variable fields
+ * dynamically.
+ */
 typedef struct arp_packet
 {
-	u_int16_t hrd; /* Hardware address space (e.g., Ethernet) */
-	u_int16_t pro; /* Protocol address space */
-	u_int8_t hln;  /* Byte length of each hardware address */
-	u_int8_t pln;  /* Byte length of each protocol address */
-	u_int16_t op;  /* Opcode REQUEST or REPLY */
-	u_int8_t *sha; /* Hardware address of sender of this packet (MAC) */
-	u_int8_t *spa; /* Protocol address of sender of this packet (IP) */
-	u_int8_t *tha; /* Hardware address of target of this packet (if known) */
-	u_int8_t *tpa; /* Protocol address of target (IP) */
+	/* Hardware address space (e.g., Ethernet) */
+	u_int16_t hrd;
+	/* Protocol address space */
+	u_int16_t pro;
+	/* Byte length of each hardware address */
+	u_int8_t hln;
+	/* Byte length of each protocol address */
+	u_int8_t pln;
+	/* Opcode REQUEST or REPLY */
+	u_int16_t op;
+	/* Hardware address of sender of this packet, hln bytes */
+	u_int8_t *sha;
+	/* Protocol address of sender of this packet, pln bytes */
+	u_int8_t *spa;
+	/* Hardware address of target of this packet (if known), hln bytes */
+	u_int8_t *tha;
+	/* Protocol address of target (IP), pln bytes */
+	u_int8_t *tpa;
 } arp_packet;
 
 #endif
