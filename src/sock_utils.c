9df1b7a9c303da594dda5cea840ca3622ec707bc
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-08 21:24:56 +0100

    Change params for flexibility

--- src/sock_utils.c
+++ src/sock_utils.c
@@ -4,21 +4,13 @@
 #include <netdb.h>
 #include <string.h>
 
-/**
- * @brief Gets a pointer to an addrinfo structure for the destination
- * IP address. Allows socket setup to be IP-address family agnostic.
- * The returned struct should be freed with `freeaddrinfo()`.
- *
- * @param dst IP string.
- * @return `struct addrinfo*` on success. `NULL` if an error occurs.
- */
-struct addrinfo *get_dst_addr_struct(char *dst)
+struct addrinfo *get_dst_addr_struct(char *dst, int sock_type)
 {
 	struct addrinfo *dst_info;
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_socktype = sock_type;
 	hints.ai_flags = AI_PASSIVE;
 
 	int ret = getaddrinfo(dst, NULL, &hints, &dst_info);
