55a9cfdef01d7a43c25ad4968c7febe7d8224ab8
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-08 21:25:26 +0100

    Update doc

--- include/sock_utils.h
+++ include/sock_utils.h
@@ -1,6 +1,15 @@
 #ifndef SOCK_UTILS_H
 #define SOCK_UTILS_H
 
-struct addrinfo *get_dst_addr_struct(char *);
+/**
+ * @brief Gets a pointer to an addrinfo structure for the destination
+ * IP address. Allows socket setup to be IP-address family agnostic.
+ * The returned struct should be freed with `freeaddrinfo()`.
+ *
+ * @param dst IP string.
+ * @param sock_type Socket type to be used in the `hints` struct, i.e `SOCK_RAW`.
+ * @return `struct addrinfo*` on success. `NULL` if an error occurs.
+ */
+struct addrinfo *get_dst_addr_struct(char *, int);
 
 #endif
