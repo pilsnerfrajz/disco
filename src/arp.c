8727b294f83ea7b66c47dfc6844e41e77280d0ed
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-09 20:24:08 +0100

    Get mask, ip and mac from if

--- src/arp.c
+++ src/arp.c
@@ -25,6 +25,8 @@ int arp(char *address)
 		return -1;
 	}
 
+	// pcap_inject();
+
 	/*struct addrinfo *dst_info = get_dst_addr_struct(address, SOCK_RAW);
 	if (dst_info == NULL)
 	{
@@ -46,26 +48,90 @@ int get_mac_addr(void)
 		perror("getifaddrs");
 		return STRUCT_ERROR;
 	}
-	// TODO Use getsockname to get the IP.
-	// TODO Combine with this code to get the correct interface
-	// TODO Check that MAC and IP belong to the same interface
+	/*
+	 * Get MAC, IP, subnet mask from interface. Check if source and target are
+	 * on the same subnet.
+	 * If not continue until found or list is null.
+	 * Else save IP and MAC.
+	 */
+	int net_mask = 0;
+	int ip_addr = 0;
+	int mac_addr = 0;
+	char *iface = "";
+	unsigned char *mac;
+	struct sockaddr_in *ip;
+	struct sockaddr_in *mask;
+
 	for (ifa = ifap; ifa != NULL; ifa = ifa->ifa_next)
 	{
+		// check if loopback and continue
+		/*if (ip != NULL && ip->sin_addr.s_addr == htonl(INADDR_LOOPBACK))
+		{
+			continue;
+		}*/
+
+		if (ip_addr && mac_addr && net_mask)
+		{
+			printf("Interface: %s\n", iface);
+
+			char print_ip[INET_ADDRSTRLEN];
+			if (inet_ntop(AF_INET, &ip->sin_addr.s_addr, print_ip, INET_ADDRSTRLEN) == NULL)
+			{
+				perror("inet_ntop");
+			}
+			printf("IP: %s\n", print_ip);
+
+			char print_mask[INET_ADDRSTRLEN];
+			if (inet_ntop(AF_INET, &mask->sin_addr, print_mask, INET_ADDRSTRLEN) == NULL)
+			{
+				perror("inet_ntop");
+			}
+			printf("MASK: %s\n", print_mask);
+
+			printf("MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+				   mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+			break;
+		}
+
+		if (ifa->ifa_name != NULL)
+		{
+			if (strncmp(iface, ifa->ifa_name, strlen(iface)) != 0)
+			{
+				iface = ifa->ifa_name;
+				net_mask = 0;
+				ip_addr = 0;
+				mac_addr = 0;
+			}
+		}
+
 		/* AF_LINK = macOS interface */
 		if (ifa->ifa_addr != NULL && ifa->ifa_addr->sa_family == AF_LINK)
 		{
 			/* https://www.illumos.org/man/3SOCKET/sockaddr_dl */
 			struct sockaddr_dl *s = (struct sockaddr_dl *)ifa->ifa_addr;
 
-			/* Jump to address in sdl_data with macro */
-			unsigned char *mac = (unsigned char *)LLADDR(s);
-
 			if (s->sdl_type == IFT_ETHER)
 			{
-				printf("%s\t", ifa->ifa_name);
-				printf("MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
-					   mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+				// iface = ifa->ifa_name;
+				//  printf("%s\n", ifa->ifa_name);
+				/* Jump to address in sdl_data with macro */
+				mac = (unsigned char *)LLADDR(s);
+				mac_addr = 1;
 			}
+		}
+
+		/* Add Linux support ^ */
+
+		if (ifa->ifa_netmask != NULL && ifa->ifa_netmask->sa_family == AF_INET)
+		{
+			mask = (struct sockaddr_in *)(ifa->ifa_netmask);
+			net_mask = 1;
+		}
+
+		if (ifa->ifa_addr != NULL && ifa->ifa_addr->sa_family == AF_INET)
+		{
+			ip = (struct sockaddr_in *)ifa->ifa_addr;
+			ip_addr = 1;
 		}
 	}
 
