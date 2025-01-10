8a1d1ab63677ae8580e4976c6eacf917950fb890
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-10 20:55:56 +0100

    Make ARP test same as ping test

--- tests/arp_test.c
+++ tests/arp_test.c
@@ -3,12 +3,15 @@
 
 void arp_test(void)
 {
+	int ret;
 	printf("-- ARP TESTS --\n");
-	printf("\tARP possible test\n");
-	int ret = arp("192.168.1.100");
-	printf("ARP good test: %d\n", ret);
+	if ((ret = arp("192.168.1.100") == 0))
+		printf("✅ Local IP supports ARP test passed\n");
+	else
+		printf("❌ Local IP supports ARP test failed. Return code: %d\n", ret);
 
-	printf("\tARP not possible test\n");
-	ret = arp("8.8.8.8");
-	printf("ARP bad test: %d\n", ret);
+	if ((ret = arp("8.8.8.8") != 0))
+		printf("✅ Remote IP does not support ARP test passed\n");
+	else
+		printf("❌ Remote IP does not support ARP test failed. Return code: %d\n", ret);
 }
