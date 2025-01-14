11dda33f57069951159aecdb27939d9fb4921620
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-14 21:52:48 +0100

    Updates dst_addr docs

--- include/sock_utils.h
+++ include/sock_utils.h
@@ -4,13 +4,22 @@
 /**
  * @brief Gets a pointer to an addrinfo structure for the destination
  * IP address. Allows socket setup to be IP-address family agnostic.
- * The returned struct should be freed with `freeaddrinfo()`.
+ * The only fields available in the struct are `ai_addr`, `ai_addrlen`
+ * and `ai_family`. The other fields are not initialized.
+ * The returned struct should be freed with `free_dst_addr_struct`.
  *
  * @param dst IP string.
  * @param sock_type Socket type to be used in the `hints` struct, i.e `SOCK_RAW`.
  * @return `struct addrinfo*` on success. `NULL` if an error occurs.
  */
 struct addrinfo *get_dst_addr_struct(char *, int);
+
+/**
+ * @brief Frees the struct returned by `get_dst_addr_struct()`.
+ *
+ * @param dst Struct returned by the `get_dst_addr_struct()`.
+ */
+void free_dst_addr_struct(struct addrinfo *);
 
 /**
  * @brief Prints IPv4 addresses for debugging.
