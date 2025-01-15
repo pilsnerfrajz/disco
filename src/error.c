ee6e59982b60ce3569b5263e862fac1ae9ff7fa7
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-01-15 22:36:00 +0100

    Add ARP specific errors

--- src/error.c
+++ src/error.c
@@ -6,9 +6,18 @@ const char *const error_strings[] = {
 	"Success",
 	"No response from host",
 	"Invalid or unknown host",
-	"STRUCT_ERROR",
 	"Error during socket operation",
 	"Error during protocol lookup",
+	"Error when allocating dynamic memory",
+	"Error during interface lookup",
+	"Buffers are not large enough",
+	"Target or interface does not support ARP",
+	"Host supports ARP",
+	"pcap_init error",
+	"pcap_open_live error",
+	"pcap_inject error",
+	"pcap_filter error",
+	"pcap_loop error",
 };
 
 static_assert(ENUMS == COUNT, "Enums and err strings are not equal.\n");
