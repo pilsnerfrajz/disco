f1aafcc124a7b0ea8e6881fd6f541908847adcdf
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-02-06 19:49:45 +0100

    Add socket setup

--- src/syn_scan.c
+++ src/syn_scan.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <errno.h>
 
+#include "../include/utils.h"
 #include "../include/error.h"
 
-int port_scan()
+int port_scan(char *address)
 {
+	struct addrinfo *dst = get_dst_addr_struct(address, SOCK_RAW);
+	if (dst == NULL)
+	{
+		return UNKNOWN_HOST;
+	}
+
+	struct protoent *protocol = getprotobyname("tcp");
+	if (protocol == NULL)
+	{
+		free_dst_addr_struct(dst);
+		return PROTO_NOT_FOUND;
+	}
+
+	int sfd = socket(dst->ai_family, SOCK_RAW, protocol->p_proto);
+	if (sfd == -1)
+	{
+		free_dst_addr_struct(dst);
+		if (errno == EPERM)
+		{
+			return PERMISSION_ERROR;
+		}
+		return SOCKET_ERROR;
+	}
+
+	free_dst_addr_struct(dst);
 
 	return SUCCESS;
 }
