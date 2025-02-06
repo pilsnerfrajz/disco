65cf04113ce33bd389ec401d88d6837ef85d7a25
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-02-06 21:03:28 +0100

    Start filling out tcp header fields. Add planning

--- src/syn_scan.c
+++ src/syn_scan.c
@@ -3,9 +3,11 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
+#include <string.h>
 
 #include "../include/utils.h"
 #include "../include/error.h"
+#include "../include/syn_scan.h"
 
 int port_scan(char *address)
 {
@@ -32,6 +34,31 @@ int port_scan(char *address)
 		}
 		return SOCKET_ERROR;
 	}
+
+	// set socket options to timeout if no response is seen
+
+	// TODO Fix IP header as well for Linux support
+
+	/* Declare structs and init all fields to 0 */
+	tcp_header_t tcp_hdr;
+	tcp_flags_t flags;
+	memset(&tcp_hdr, 0, sizeof(tcp_header_t));
+	memset(&flags, 0, sizeof(tcp_flags_t));
+
+	flags.syn = 1;
+	tcp_hdr.flags = *(u_int8_t *)&flags;
+	tcp_hdr.offset_rsrvd.offset = 5;
+	// set rest of fields
+
+	// send to first port
+
+	// wait for answer and check RST or SYN-ACK
+
+	// print or save results
+
+	// change port number and maybe src port
+
+	// send and repeat
 
 	free_dst_addr_struct(dst);
 
