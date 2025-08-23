92bf81aa8f21a4bc2a569054e1f91808561768fa
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-08-23 14:52:51 +0200

    Add description for port_scan

--- include/syn_scan.h
+++ include/syn_scan.h
@@ -1,7 +1,27 @@
 #ifndef SYN_SCAN_H
 #define SYN_SCAN_H
 
-int port_scan(char *address, unsigned short *port_arr, int count, int print_state);
+/**
+ * @brief Performs a SYN scan on the specified ports of a target address or
+ * domain. If `print_state` is true, the open ports will be printed. An array
+ * containing the status of all scanned ports is available via the pointer
+ * `result_arr`. `result_arr` should be freed with `free()`.
+ *
+ * Specifying a larger `count` than elements in `port_arr` will lead to
+ * undefined behavior.
+ *
+ * @param address The target address to scan.
+ * @param port_arr The array of ports to scan.
+ * @param count The number of ports in the array.
+ * @param print_state Whether to print the open ports.
+ * @param result_arr Pointer to an array to store the results of the scan.
+ * @return `int` Returns SUCCESS on success, or an error code from `error.h` on failure.
+ */
+int port_scan(char *address,
+			  unsigned short *port_arr,
+			  int count,
+			  int print_state,
+			  short **result_arr);
 
 /**
  * @brief Parses a string with a format similar to `"1,2,3-5,6"`, and returns it
