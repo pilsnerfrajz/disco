316d6db920ba4d904473de9f5a353b53be3894fd
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-08-25 00:04:26 +0200

    Update CLI header with usage function and parse_cli return type

--- include/cli.h
+++ include/cli.h
@@ -1,12 +1,13 @@
 #ifndef CLI_H
 #define CLI_H
 
-void parse_cli(int argc,
-			   char *argv[],
-			   char **target,
-			   char **ports,
-			   int *no_host_disc,
-			   int *force_ping,
-			   int *force_arp);
+void usage(FILE *stream);
+int parse_cli(int argc,
+			  char *argv[],
+			  char **target,
+			  char **ports,
+			  int *no_host_disc,
+			  int *force_ping,
+			  int *force_arp);
 
 #endif
