efe574c926032758efa7c28ab2476ef358a1aa5f
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-08-30 23:28:48 +0200

    Add show_open support for tests

--- tests/cli_test.c
+++ tests/cli_test.c
@@ -3,10 +3,15 @@
 #include "../include/cli.h"
 #include "../include/error.h"
 
-static void test(int number_of_flags, char *ports, char *target, int no_host_disc, int force_ping, int force_arp)
+static void test(int number_of_flags, char *ports, int show_open, char *target, int no_host_disc, int force_ping, int force_arp)
 {
 	int set = 0;
 	if (ports)
+	{
+		set++;
+	}
+
+	if (show_open)
 	{
 		set++;
 	}
@@ -59,6 +64,7 @@ void cli_test(void)
 	int no_host_disc = 0;
 	int force_ping = 0;
 	int force_arp = 0;
+	int show_open = 0;
 
 	char *help[] = {"program", "-h"};
 	char *parse[] = {"program",
@@ -66,18 +72,19 @@ void cli_test(void)
 					 "80,443",
 					 "--no-check",
 					 "-P",
+					 "-o",
 					 "-a",
 					 "invalid",
 					 "127.0.0.1",
 					 "::1"};
 
-	parse_cli(2, help, &target, &ports, &no_host_disc, &force_ping, &force_arp);
+	parse_cli(2, help, &target, &ports, &show_open, &no_host_disc, &force_ping, &force_arp);
 
-	test(0, ports, target, no_host_disc, force_ping, force_arp);
+	test(0, ports, 0, target, no_host_disc, force_ping, force_arp);
 
-	parse_cli(10, parse, &target, &ports, &no_host_disc, &force_ping, &force_arp);
+	parse_cli(11, parse, &target, &ports, &show_open, &no_host_disc, &force_ping, &force_arp);
 
-	test(5, ports, target, no_host_disc, force_ping, force_arp);
+	test(6, ports, 1, target, no_host_disc, force_ping, force_arp);
 
 	free(ports);
 	free(target);
