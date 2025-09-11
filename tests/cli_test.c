0800a38d804f09ffdc852ac5102317706b1141a4
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-09-11 21:47:44 +0200

    Update tests to accomodate -S flag

--- tests/cli_test.c
+++ tests/cli_test.c
@@ -64,6 +64,7 @@ void cli_test(void)
 	int no_host_disc = 0;
 	int force_ping = 0;
 	int force_arp = 0;
+	int force_syn = 0;
 	int show_open = 0;
 
 	char *help[] = {"program", "-h"};
@@ -78,11 +79,11 @@ void cli_test(void)
 					 "127.0.0.1",
 					 "::1"};
 
-	parse_cli(2, help, &target, &ports, &show_open, &no_host_disc, &force_ping, &force_arp, NULL);
+	parse_cli(2, help, &target, &ports, &show_open, &no_host_disc, &force_ping, &force_arp, &force_syn, NULL);
 
 	test(0, ports, 0, target, no_host_disc, force_ping, force_arp);
 
-	parse_cli(11, parse, &target, &ports, &show_open, &no_host_disc, &force_ping, &force_arp, NULL);
+	parse_cli(11, parse, &target, &ports, &show_open, &no_host_disc, &force_ping, &force_arp, &force_syn, NULL);
 
 	test(6, ports, 1, target, no_host_disc, force_ping, force_arp);
 
