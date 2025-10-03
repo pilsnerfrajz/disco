81a8b5e3ac18442105bccfeec35ee813e6456212
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-10-03 23:48:30 +0200

    Add OS constants

--- include/fingerprint.h
+++ include/fingerprint.h
@@ -3,6 +3,19 @@
 
 #include <sys/types.h>
 
+#define UNKNOWN_OS 1
+#define LINUX_OS 2
+#define MAC_BSD_OS 3
+#define WINDOWS_OS 4
+#define ROUTER_OS 5
+
+#define LINUX_TTL 64
+#define MAC_BSD_TTL 64
+#define WINDOWS_TTL 128
+#define ROUTER_TTL 255
+
+#define MAC_BSD_WIN_SIZE 65535
+
 struct fingerprint
 {
 	u_int8_t ttl;
