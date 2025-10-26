11a7c56a71130c9954efc85bd99cdeb950320b22
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-10-26 22:22:50 +0100

    Add finger tests to test suite

--- tests/run_all_tests.c
+++ tests/run_all_tests.c
@@ -3,6 +3,7 @@
 #include "include/arp_test.h"
 #include "include/ping_test.h"
 #include "include/syn_scan_test.h"
+#include "include/fingerprint_test.h"
 #include "include/cli_test.h"
 
 int main(void)
@@ -12,6 +13,8 @@ int main(void)
 	arp_test();
 	printf("\n");
 	syn_scan_test();
+	printf("\n");
+	fingerprint_tests();
 	printf("\n");
 	cli_test();
 
