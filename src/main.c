07218dea3d893f3e47cb90115f00293504cd842b
williamhedenskog <94441813+pilsnerfrajz@users.noreply.github.com>
2025-10-26 22:58:29 +0100

    Check for actual network distance

--- src/main.c
+++ src/main.c
@@ -337,14 +337,20 @@ int main(int argc, char *argv[])
 				goto cleanup;
 			}
 		}
-		finger.ttl = target_info.ttl;
-		finger.window_size = target_info.window_size;
-		int os = determine_os(&finger);
-		print_os(os, fp);
-		int hops = network_dist(os, finger.ttl);
-		snprintf(msg_buf, MSG_BUF_SIZE, "[+] Network distance (estimate in hops): %d\n", hops);
-		print_wrapper(stdout, fp, msg_buf);
-		memset(msg_buf, 0, MSG_BUF_SIZE);
+		if (target_info.is_up)
+		{
+			finger.ttl = target_info.ttl;
+			finger.window_size = target_info.window_size;
+			int os = determine_os(&finger);
+			print_os(os, fp);
+			if (target_info.is_open_port)
+			{
+				int hops = network_dist(os, finger.ttl);
+				snprintf(msg_buf, MSG_BUF_SIZE, "[+] Network distance (estimate in hops): %d\n", hops);
+				print_wrapper(stdout, fp, msg_buf);
+				memset(msg_buf, 0, MSG_BUF_SIZE);
+			}
+		}
 	}
 
 	if (ports != NULL)
@@ -384,14 +390,20 @@ int main(int argc, char *argv[])
 			print_wrapper(stdout, fp, msg);
 		}
 
-		finger.ttl = target_info.ttl;
-		finger.window_size = target_info.window_size;
-		int os = determine_os(&finger);
-		print_os(os, fp);
-		int hops = network_dist(os, finger.ttl);
-		snprintf(msg_buf, MSG_BUF_SIZE, "[+] Network distance (estimate in hops): %d\n", hops);
-		print_wrapper(stdout, fp, msg_buf);
-		memset(msg_buf, 0, MSG_BUF_SIZE);
+		if (target_info.is_up)
+		{
+			finger.ttl = target_info.ttl;
+			finger.window_size = target_info.window_size;
+			int os = determine_os(&finger);
+			print_os(os, fp);
+			if (target_info.is_open_port)
+			{
+				int hops = network_dist(os, finger.ttl);
+				snprintf(msg_buf, MSG_BUF_SIZE, "[+] Network distance (estimate in hops): %d\n", hops);
+				print_wrapper(stdout, fp, msg_buf);
+				memset(msg_buf, 0, MSG_BUF_SIZE);
+			}
+		}
 	}
 
 	if (fp != NULL)
